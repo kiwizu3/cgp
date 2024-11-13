554532cb10c81851c96d6a0d20de9759024d9544
kiwi <kavindasu@gmail.com>
2024-11-14 02:55:48 +0530

    Facts style update

--- src/components/Facts.js
+++ src/components/Facts.js
@@ -6,26 +6,26 @@ function Facts() {
       <div className="container">
         <div className="row text-center g-4">
           <div className="col-lg-3 col-md-6">
-            <div className="fact-item p-4 border rounded shadow-sm">
-              <h2>Qualified Team</h2>
+            <div className="fact-item py-5 px-4 rounded shadow">
+              <h4 className="fw-bold">Qualified Team</h4>
               <p>Over 40 qualified professionals</p>
             </div>
           </div>
           <div className="col-lg-3 col-md-6">
-            <div className="fact-item p-4 border rounded shadow-sm">
-              <h2>Individual Approach</h2>
+            <div className="fact-item py-5 px-4 rounded shadow">
+              <h4 className="fw-bold">Individual Approach</h4>
               <p>Tailored strategies for each client</p>
             </div>
           </div>
           <div className="col-lg-3 col-md-6">
-            <div className="fact-item p-4 border rounded shadow-sm">
-              <h2>100% Success</h2>
+            <div className="fact-item py-5 px-4 rounded shadow">
+              <h4 className="fw-bold">100%<br/> Success</h4>
               <p>Committed to achieving investment success</p>
             </div>
           </div>
           <div className="col-lg-3 col-md-6">
-            <div className="fact-item p-4 border rounded shadow-sm">
-              <h2>100% Satisfaction</h2>
+            <div className="fact-item py-5 px-4 rounded shadow">
+              <h4 className="fw-bold">100% Satisfaction</h4>
               <p>Dedicated to client satisfaction</p>
             </div>
           </div>
