83c47c554465f0e8492e070d3947ba104360ecf2
kiwi <kavindasu@gmail.com>
2024-11-17 02:47:16 +0530

    Font size update

--- src/components/Facts.js
+++ src/components/Facts.js
@@ -7,25 +7,25 @@ function Facts() {
         <div className="row text-center g-4">
           <div className="col-lg-3 col-md-6">
             <div className="fact-item py-5 px-4 rounded shadow">
-              <h4 className="fw-bold">Qualified Team</h4>
+              <h5 className="fw-bold">Qualified Team</h5>
               <p>Over 40 qualified professionals</p>
             </div>
           </div>
           <div className="col-lg-3 col-md-6">
             <div className="fact-item py-5 px-4 rounded shadow">
-              <h4 className="fw-bold">Individual Approach</h4>
+              <h5 className="fw-bold">Individual Approach</h5>
               <p>Tailored strategies for each client</p>
             </div>
           </div>
           <div className="col-lg-3 col-md-6">
             <div className="fact-item py-5 px-4 rounded shadow">
-              <h4 className="fw-bold">100%<br/> Success</h4>
+              <h5 className="fw-bold">100%<br/> Success</h5>
               <p>Committed to achieving investment success</p>
             </div>
           </div>
           <div className="col-lg-3 col-md-6">
             <div className="fact-item py-5 px-4 rounded shadow">
-              <h4 className="fw-bold">100% Satisfaction</h4>
+              <h5 className="fw-bold">100% Satisfaction</h5>
               <p>Dedicated to client satisfaction</p>
             </div>
           </div>
